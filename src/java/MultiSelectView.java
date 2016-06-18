3957b35f41965705bb6bc86ba636a37034747733
omar <omar@localhost.localdomain>
2016-06-18 15:43:37 -0500

    los querubines

--- src/java/MultiSelectView.java
+++ src/java/MultiSelectView.java
@@ -4,49 +4,58 @@
  * and open the template in the editor.
  */
 
+import entities.CatActividad;
+import facade.CatActFacade;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.model.SelectItem;
 import javax.faces.model.SelectItemGroup;
- 
+import javax.inject.Inject;
+
 @ManagedBean
 public class MultiSelectView {
-     
-    private List<SelectItem> categories;    
+
+    private List<SelectItem> categories;
+    private List<SelectItem> lista;
     private String selection;
- 
+    private CatActividad catac = new CatActividad();
+    @Inject
+    private CatActFacade cat;
+
     @PostConstruct
     public void init() {
         categories = new ArrayList<SelectItem>();
-        
+        lista = new ArrayList<SelectItem>();
         SelectItemGroup Deportivo = new SelectItemGroup("Deportivo");
         SelectItemGroup Cultural = new SelectItemGroup("Cultural");
-               
-                 
-        SelectItem Ajedrez = new SelectItem("Ajedrez");
-        SelectItem Basquetbol = new SelectItem("Basquetbol");
-        SelectItem Futbol = new SelectItem("Futbol");
-        SelectItem Natacion = new SelectItem("Natación");
-         
+
+        List<CatActividad> obj = cat.findAll();
+        Iterator<CatActividad> it = obj.iterator();
+
+        for (int i = 0; i < obj.size(); i++) {
+            SelectItem tmp = new SelectItem(it.next().getNombre());
+            System.out.println("TMP1: " + tmp.getLabel());
+            Deportivo.setSelectItems(new SelectItem[]{tmp});
+
+        }
         SelectItem Escolta = new SelectItem("Escolta");
-                                            
-        Deportivo.setSelectItems(new SelectItem[]{Ajedrez, Basquetbol, Futbol, Natacion});
         Cultural.setSelectItems(new SelectItem[]{Escolta});
-         
-        
+
         categories.add(Deportivo);
         categories.add(Cultural);
     }
-     
+
     public List<SelectItem> getCategories() {
         return categories;
-    }    
- 
+    }
+
     public String getSelection() {
         return selection;
     }
+
     public void setSelection(String selection) {
         this.selection = selection;
     }
