c551ac6bdee5f07695d52fbe9175136ec919f143
omar <omar@localhost.localdomain>
2016-06-29 18:19:05 -0500

    los querubines

--- src/java/entities/CatActividad.java
+++ src/java/entities/CatActividad.java
@@ -6,9 +6,7 @@
 package entities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,16 +14,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author pablog23
+ * @author omar
  */
 @Entity
 @Table(name = "CatActividad")
@@ -36,22 +32,17 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "CatActividad.findByNombre", query = "SELECT c FROM CatActividad c WHERE c.nombre = :nombre"),
     @NamedQuery(name = "CatActividad.findByTipo", query = "SELECT c FROM CatActividad c WHERE c.tipo = :tipo")})
 public class CatActividad implements Serializable {
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "catActividad")
-    private Collection<Actividad> actividadCollection;
     private static final long serialVersionUID = 1L;
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "idCatAct")
     private Integer idCatAct;
-    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 45)
     @Column(name = "nombre")
     private String nombre;
-    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 45)
@@ -118,15 +109,6 @@ public class CatActividad implements Serializable {
     @Override
     public String toString() {
         return "entities.CatActividad[ idCatAct=" + idCatAct + " ]";
-    }
-
-    @XmlTransient
-    public Collection<Actividad> getActividadCollection() {
-        return actividadCollection;
-    }
-
-    public void setActividadCollection(Collection<Actividad> actividadCollection) {
-        this.actividadCollection = actividadCollection;
     }
     
 }
